f90fd0ef0146cea38f8ddfc8371cf67f59b0c637
lc-code20 <wyc90627694@gmail.com>
2021-09-29 10:03:17 +0100

    add index and js

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+console.log(":)")
+
+const firstName = "L";
+
+const sureName = "C";
+
+const from = "UK";
+
+const horoscope = "Scorpio";
+
+console.log(firstName);
+console.log(sureName);
+console.log(from);
+console.log(horoscope);
+
+console.log("%c" + firstName, `color: orange; font: fantasy; weight: bold;
+    background-color: black; padding: 10px`)
