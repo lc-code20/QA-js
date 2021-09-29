7c432d03308f4f5f80de05674f75ca71ed98df41
lc-code20 <wyc90627694@gmail.com>
2021-09-29 11:11:35 +0100

    use template literal

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,26 @@
-console.log(":)")
+// console.log(":)")
 
-const firstName = "L";
+// const firstName = "L";
 
-const sureName = "C";
+// const sureName = "C";
 
-const from = "UK";
+// const from = "UK";
 
-const horoscope = "Scorpio";
+// const horoscope = "Scorpio";
 
-console.log(firstName);
-console.log(sureName);
-console.log(from);
-console.log(horoscope);
+// console.log(firstName);
+// console.log(sureName);
+// console.log(from);
+// console.log(horoscope);
 
-console.log("%c" + firstName, `color: orange; font: fantasy; font-weight: bold;
-    background-color: black; padding: 10px`)
+// console.log("%c" + firstName, `color: orange; font: fantasy; font-weight: bold;
+//     background-color: black; padding: 10px`);
+
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+//`The total bill is £4000 the remaining amount of money to be paid is £1652`
+
+console.log(`The total bill is £${totalMoney}, the remaining amount of money to be paid is £${totalLeftToPay}`)
